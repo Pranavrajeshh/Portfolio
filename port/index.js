06d6d81ced8559175be49dbc8fb21dda7ea14bb7
Pranavrajeshh <107356266+Pranavrajeshh@users.noreply.github.com>
2023-07-06 13:51:33 +0530

    Add files via upload

---
+++ port/index.js
@@ -0,0 +1,27 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll",function() {
+  header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('active');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navlist.classList.remove('active');
+};
+
+const sr = ScrollReveal ({
+  distance: '45px', 
+  duration: 500, 
+  reset: true
+})
+sr.reveal('.home-text',{delay:350, origin:'left'})
+sr.reveal('.homeimg',{delay:350, origin:'right'})
+
+sr.reveal('.about, .projects, .skills, .contact',{delay:300, origin:'bottom'})
